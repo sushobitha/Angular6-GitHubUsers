7015766a32ad364770764a9dc3e335be3680adae
sushobitha <sushobhithaps@gmail.com>
2018-10-20 01:23:16 +0530

    changes to server.js

--- server.js
+++ server.js
@@ -5,12 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/<name-of-app>'));
+app.use(express.static(__dirname + '/dist/Angular6-GitHubUsers'));
 
-app.get('/*', function(req,res) {
-    
-res.sendFile(path.join(__dirname+'/dist/<name-of-app>/index.html'));
-});
+app.get('*', (req, res) => {
+    res.sendFile(`./Angular6-GitHubUsers/dist/index.html`); // load the single view file (angular will handle the page changes on the front-end)
+})
 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
